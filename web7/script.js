f0deeeee7c14a3e7f4fc287dd03abdfb51ccbcb5
VelVit24 <61752135+VelVit24@users.noreply.github.com>
2023-10-26 19:21:27 +0300

    Add files via upload

--- web7/script.js
+++ web7/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $('.slider').slick({
-        //variableWidth: true,
         prevArrow:"<img class='a-left control-c prev slick-prev' src='images/left-arrow.jpg'>",
         nextArrow:"<img class='a-right control-c next slick-next' src='images/right-arrow.jpg'>",
         dots: true,

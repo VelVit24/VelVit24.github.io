e5a9e8f4005e2c8daa6759846724ba9f89aed008
Виталий Величко <vitaliy@MacBook-Air-Vitalij.local>
2024-02-10 16:33:51 +0300

    ri

---
+++ web_frontend/web7/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='images/left-arrow.jpg'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='images/right-arrow.jpg'>",
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+})

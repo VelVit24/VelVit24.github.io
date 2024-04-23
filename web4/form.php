c3169019741033a54fcaab075e58da72b2521975
VelVit24 <vel_vit26@mail.ru>
2024-04-23 23:01:53 +0300

    1

--- web4/form.php
+++ web4/form.php
@@ -1,31 +1,82 @@
 <html>
-  <head>
-    <style>
-/* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
-.error {
-  border: 2px solid red;
-}
-    </style>
-  </head>
-  <body>
+    <head>
+        <title>Form</title>
+        <meta charset="utf-8">
 
-<?php
-if (!empty($messages)) {
-  print('<div id="messages">');
-  // Выводим все сообщения.
-  foreach ($messages as $message) {
-    print($message);
-  }
-  print('</div>');
-}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-// Далее выводим форму отмечая элементы с ошибками классом error
-// и задавая начальные значения элементов ранее сохраненными.
-?>
+        <style>
+            /* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
+            .error {
+              border: 2px solid red;
+            }
+        </style>
+    </head>
+<body>
+<div class="header">
+    <h1>Drupal</h1>
+</div>
+<div class="container">
+    <div class="cont">
+        <div class="title">
+            <h2>Оставить заявку для связи с нами</h2>
+        </div>
+        <?php
+        if (!empty($messages)) {
+            print('<div id="messages">');
+            // Выводим все сообщения.
+            foreach ($messages as $message) {
+                print($message);
+            }
+            print('</div>');
+        }
 
-    <form action="" method="POST">
-      <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
-      <input type="submit" value="ok" />
-    </form>
-  </body>
+        // Далее выводим форму отмечая элементы с ошибками классом error
+        // и задавая начальные значения элементов ранее сохраненными.
+        ?>
+        <form action="" method="POST">
+            <label>Ваше ФИО<br/>
+                <input name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>">
+            </label><br/>
+            <label>Номер телефона<br/>
+                <input name="phone" <?php if ($errors['phone']) {print 'class="error"';} ?> value="<?php print $values['phone']; ?>">
+            </label><br/>
+            <label>E-mail<br/>
+                <input name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>">
+            </label><br/>
+            <label>Дата рождения<br/>
+                <input name="birthday" <?php if ($errors['date']) {print 'class="error"';} ?> value="<?php print $values['date']; ?>">
+            </label><br/>
+            <label>Пол<br/>
+                <input type="radio" name="gender" value="male" checked>  Мужской<br/>
+                <input type="radio" name="gender" value="female">  Женский
+            </label><br/>
+            <label>Языки программирования:<br/>
+                <select multiple name="languages[]">
+                    <option value="1">Pascal</option>
+                    <option value="2">C</option>
+                    <option value="3">C++</option>
+                    <option value="4">JavaScript</option>
+                    <option value="5">PHP</option>
+                    <option value="6">Python</option>
+                    <option value="7">Java</option>
+                    <option value="8">Haskel</option>
+                    <option value="9">Clojure</option>
+                    <option value="10">Prolog</option>
+                    <option value="11">Scala</option>
+                </select>
+            </label><br/>
+            <label>Биография<br/>
+                <textarea name="biography" <?php if ($errors['bio']) {print 'class="error"';} ?> ><?php print $values['bio']; ?></textarea>
+            </label><br/>
+            <label for="cb" class="label-checkbox"  <?php if ($errors['check']) {print 'class="error"';} ?>>
+                <input name="check" type="checkbox" id="cb"> С контрактом ознакомлен(а).
+            </label><br/>
+            <input type="submit" value="Сохранить">
+        </form>
+    </div>
+</div>
+</body>
+
 </html>
+

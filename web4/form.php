d05809582271bd31446ead636ec45375db89d5b8
VelVit24 <vel_vit26@mail.ru>
2024-04-24 15:33:43 +0300

    1

--- web4/form.php
+++ web4/form.php
@@ -4,12 +4,7 @@
         <meta charset="utf-8">
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-        <style>
-            .error {
-              border: 2px solid red;
-            }
-        </style>
+        <link rel="stylesheet" href='style.css' type="text/css">
     </head>
 <body>
 <div class="header">

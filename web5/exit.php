6983348bf86b397dc93b89643b1f4156440b8de1
VelVit24 <vel_vit26@mail.ru>
2024-04-28 23:07:22 +0300

    1

--- web5/exit.php
+++ web5/exit.php
@@ -1,7 +1,10 @@
 <?php
 if (isset($_POST['act_exit'])) {
     setcookie(session_name(), '', 100000);
+    $_SESSION = array();
     session_destroy();
+    setcookie('login', '', 100000);
+    setcookie('pass', '', 100000);
     setcookie('name_value', '', 100000);
     setcookie('phone_value', '', 100000);
     setcookie('email_value', '', 100000);

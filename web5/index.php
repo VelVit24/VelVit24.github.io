50e1df2f3742a6ac47cb2dceaa5603295f90f3cf
VelVit24 <vel_vit26@mail.ru>
2024-04-29 10:39:25 +0300

    1

--- web5/index.php
+++ web5/index.php
@@ -123,10 +123,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     // Если нет предыдущих ошибок ввода, есть кука сессии, начали сессию и
     // ранее в сессию записан факт успешного логина.
-    var_dump($errors);
-    var_dump($_COOKIE[session_name()]);
-    var_dump($_SESSION['login']);
-    if (empty($errors) && !empty($_COOKIE[session_name()]) &&
+
+    $fl = false;
+    foreach($errors as $er) if ($er) $fl = true;
+    if (!$fl && !empty($_COOKIE[session_name()]) &&
         session_start() && !empty($_SESSION['login'])) {
 
         $stmt = $db->prepare("SELECT * FROM application WHERE id_app = ?");

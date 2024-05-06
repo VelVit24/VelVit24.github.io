b22d0673df6f4bceb78f6e2c0104c82f620fcd94
VelVit24 <vel_vit26@mail.ru>
2024-05-06 11:43:45 +0300

    1

--- web6/admin_upd_form2.php
+++ web6/admin_upd_form2.php
@@ -1,13 +1,118 @@
 <?php
-    if (!empty($messages)) {
-        print('<div id="messages">');
-        foreach ($messages as $message) {
-            print($message);
-        }
-        print('</div>');
+$messages = array();
+
+$errors = array();
+$errors['name'] = !empty($_COOKIE['name_error']);
+$errors['phone'] = !empty($_COOKIE['phone_error']);
+$errors['email'] = !empty($_COOKIE['email_error']);
+$errors['date'] = !empty($_COOKIE['date_error']);
+$errors['gender'] = !empty($_COOKIE['gen_error']);
+$errors['bio'] = !empty($_COOKIE['bio_error']);
+$errors['lang'] = !empty($_COOKIE['lang_error']);
+$errors['check'] = !empty($_COOKIE['check_error']);
+
+
+if ($errors['name']) {
+    if ($_COOKIE['name_error'] == 1)
+        $messages[] = '<div class="error">Заполните имя.</div>';
+    if ($_COOKIE['name_error'] == 2)
+        $messages[] = '<div class="error">Длина имени должна быть от 1 до 150 символов.</div>';
+    if ($_COOKIE['name_error'] == 3)
+        $messages[] = '<div class="error">Имя должно содержать только русские буквы и пробелы.</div>';
+    setcookie('name_error', '', 100000);
+}
+if ($errors['phone']) {
+    if ($_COOKIE['phone_error'] == 1)
+        $messages[] = '<div class="error">Заполните номер телефона.</div>';
+    if ($_COOKIE['phone_error'] == 2)
+        $messages[] = '<div class="error">Неправильный номер телефона: номер должен состоять из 11 цифр.</div>';
+    if ($_COOKIE['phone_error'] == 3)
+        $messages[] = '<div class="error">Неправильный номер телефона: номер должен начинаться с 7 или 8 и состоять только из цифр.</div>';
+    setcookie('phone_error', '', 100000);
+}
+if ($errors['email']) {
+    if ($_COOKIE['email_error'] == 1)
+        $messages[] = '<div class="error">Заполните Email.</div>';
+    if ($_COOKIE['email_error'] == 2)
+        $messages[] = '<div class="error">Длина Email должна быть от 5 до 256 символов.</div>';
+    if ($_COOKIE['email_error'] == 3)
+        $messages[] = '<div class="error">Неправильный Email: должен содержать только строчные латинские буквы и символы _-.@</div>';
+    setcookie('email_error', '', 100000);
+}
+if ($errors['date']) {
+    if ($_COOKIE['date_error'] == 1)
+        $messages[] = '<div class="error">Заполните дату рождения.</div>';
+    if ($_COOKIE['date_error'] == 2)
+        $messages[] = '<div class="error">Неправильная дата рождения: дата должна быть в формате DD.MM.YYYY</div>';
+    setcookie('date_error', '', 100000);
+}
+if ($errors['gender']) {
+    if ($_COOKIE['gen_error'] == 1)
+        $messages[] = '<div class="error">Выберите пол.</div>';
+    setcookie('gen_error', '', 100000);
+}
+if ($errors['lang']) {
+    if ($_COOKIE['lang_error'] == 1)
+        $messages[] = '<div class="error">Укажите хотя бы 1 язык программирования.</div>';
+    setcookie('lang_error', '', 100000);
+}
+if ($errors['bio']) {
+    if ($_COOKIE['bio_error'] == 1)
+        $messages[] = '<div class="error">Заполните биографию.</div>';
+    if ($_COOKIE['bio_error'] == 2)
+        $messages[] = '<div class="error">Длина биографии должна быть от 1 до 1000 символов.</div>';
+    if ($_COOKIE['bio_error'] == 3)
+        $messages[] = '<div class="error">Биография должна содержать только буквы, цифры, пробелы и символы .,-;:@%!"</div>';
+    setcookie('bio_error', '', 100000);
+}
+if ($errors['check']) {
+    if ($_COOKIE['check_error'] == 1)
+        $messages[] = '<div class="error">Отметьте галочку напротив "С контрактом ознакомлен(а)"</div>';
+    setcookie('check_error', '', 100000);
+}
+
+// Складываем предыдущие значения полей в массив, если есть.
+$values = array();
+$values['name'] = empty($_COOKIE['name_value1']) ? '' : $_COOKIE['name_value1'];
+$values['phone'] = empty($_COOKIE['phone_value1']) ? '' : $_COOKIE['phone_value1'];
+$values['email'] = empty($_COOKIE['email_value1']) ? '' : $_COOKIE['email_value1'];
+$values['date'] = empty($_COOKIE['date_value1']) ? '' : $_COOKIE['date_value1'];
+$values['gen'] = empty($_COOKIE['gen_value1']) ? '' : $_COOKIE['gen_value1'];
+$values['lang'] = empty($_COOKIE['lang_value1']) ? '' : $_COOKIE['lang_value1'];
+$values['bio'] = empty($_COOKIE['bio_value1']) ? '' : $_COOKIE['bio_value1'];
+
+$fl = false;
+foreach($errors as $er) if ($er) $fl = true;
+if (!$fl && !empty($_COOKIE[session_name()]) &&
+    session_start() && !empty($_SESSION['login'])) {
+
+    $stmt = $db->prepare("SELECT * FROM application WHERE id_app = ?");
+    $stmt->execute([$_SESSION['uid']]);
+    $data = $stmt->fetch();
+    $values['name'] = $data['name'];
+    $values['phone'] = $data['phone_number'];
+    $values['email'] = $data['email'];
+    $values['date'] = $data['birthday'];
+    $values['gen'] = $data['gender'];
+    $values['bio'] = $data['biography'];
+
+    $stmt = $db->prepare("SELECT * FROM applications_languages WHERE id_app = ?");
+    $stmt->execute([$_SESSION['uid']]);
+    $t = "||";
+    while ($row = $stmt->fetch()) {
+        $t = $t . $row['id_lang'] . "|";
     }
+    $values['lang'] = $t;
+}
+if (!empty($messages)) {
+    print('<div id="messages">');
+    foreach ($messages as $message) {
+        print($message);
+    }
+    print('</div>');
+}
 ?>
-<form action="" method="POST">
+<form action="admin_update_user.php" method="POST">
     <label>Ваше ФИО<br/>
         <input name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>">
     </label><br/>
---
+++ web6/admin_update_user.php
@@ -0,0 +1,24 @@
+<?php
+include('validate.php');
+
+if ($errors) {
+    header('Location: admin.php');
+    exit();
+} else {
+    setcookie('name_error', '', 100000);
+    setcookie('phone_error', '', 100000);
+    setcookie('email_error', '', 100000);
+    setcookie('date_error', '', 100000);
+    setcookie('gen_error', '', 100000);
+    setcookie('lang_error', '', 100000);
+    setcookie('bio_error', '', 100000);
+    setcookie('check_error', '', 100000);
+}
+
+include('db_update.php');
+
+setcookie('admin_upd', '', 100000);
+
+
+// Делаем перенаправление.
+header('Location: admin.php');

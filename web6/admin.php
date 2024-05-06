e19c83379cfeec2a206d902f7d6980444633745e
VelVit24 <vel_vit26@mail.ru>
2024-05-06 11:26:55 +0300

    1

--- web6/admin.php
+++ web6/admin.php
@@ -65,7 +65,21 @@ $row2 = $stmt2->fetchAll();
 </table>
 
 Удаление данных
-<?php include('admin_delete_db.php');
+<?php
+$messages = array();
+if (!empty($_COOKIE['del_save'])) {
+    setcookie('del_save', '', 100000);
+    $messages[] = 'Пользователь удален.';
+}
+$errors = array();
+$errors['del_id'] = !empty($_COOKIE['del_id_error']);
+if ($errors['del_id']) {
+    if ($_COOKIE['del_id_error'] == 1)
+        $messages[] = '<div class="error">Заполните ID пользователя.</div>';
+    if ($_COOKIE['del_id_error'] == 2)
+        $messages[] = '<div class="error">Такого пользователя нет.</div>';
+    setcookie('del_id_error', '', 100000);
+}
 if (!empty($messages)) {
     print('<div id="messages">');
     foreach ($messages as $message) {
@@ -74,7 +88,7 @@ if (!empty($messages)) {
     print('</div>');
 }
 ?>
-<form action="" method="POST">
+<form action="admin_delete_db.php" method="POST">
     <label> ID пользователя<br/>
     <input type="text" name="del_id" <?php if ($errors['del_id']) {print 'class="error"';} ?> >
     </label><br/>
@@ -84,9 +98,35 @@ if (!empty($messages)) {
 Редактирование данных
 <?php
 if (empty($_COOKIE['admin_upd'])) {
-    include('admin_update.php');
-    include('admin_upd_form1.php');
-}
+    $messages = array();
+    if (!empty($_COOKIE['admin_upd'])) {
+        $messages[] = 'Загружены данные пользователя '.$_COOKIE['id_upd'];
+    }
+    $errors = array();
+    $errors['upd_id'] = !empty($_COOKIE['del_id_error']);
+    if ($errors['upd_id']) {
+        if ($_COOKIE['upd_id_error'] == 1)
+            $messages[] = '<div class="error">Заполните ID пользователя.</div>';
+        if ($_COOKIE['upd_id_error'] == 2)
+            $messages[] = '<div class="error">Такого пользователя нет.</div>';
+        setcookie('upd_id_error', '', 100000);
+    }
+    if (!empty($messages)) {
+        print('<div id="messages">');
+        foreach ($messages as $message) {
+            print($message);
+        }
+        print('</div>');
+    }
+    ?>
+<form action="admin_update.php" method="POST">
+    <label> ID пользователя<br/>
+        <input type="text" name="upd_id" <?php if ($errors['upd_id']) {print 'class="error"';} ?> >
+    </label><br/>
+    <input type="submit" name="update1" value="Сохранить">
+</form>
+    <?php
+    }
 else {
     include('admin_upd_form2.php');
 }
--- web6/admin_update.php
+++ web6/admin_update.php
@@ -1,44 +1,27 @@
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $messages = array();
-    if (!empty($_COOKIE['admin_upd'])) {
-        $messages[] = 'Загружены данные пользователя '.$_COOKIE['id_upd'];
+$errors = false;
+if (empty($_POST['upd_id'])) {
+    setcookie('upd_id_error', '1', time() + 24 * 60 * 60);
+    $errors = true;
+} else {
+    $stmt = $db->query('SELECT id_app FROM application');
+    $fl = false;
+    while ($row = $stmt->fetch()) {
+        if ($row['id_app'] == $_POST['upd_id']) $fl = true;
     }
-    $errors = array();
-    $errors['upd_id'] = !empty($_COOKIE['del_id_error']);
-    if ($errors['upd_id']) {
-        if ($_COOKIE['upd_id_error'] == 1)
-            $messages[] = '<div class="error">Заполните ID пользователя.</div>';
-        if ($_COOKIE['upd_id_error'] == 2)
-            $messages[] = '<div class="error">Такого пользователя нет.</div>';
-        setcookie('upd_id_error', '', 100000);
-    }
-} else if (!empty($_POST['update1'])) {
-    $errors = false;
-    if (empty($_POST['upd_id'])) {
-        setcookie('upd_id_error', '1', time() + 24 * 60 * 60);
+    if (!$fl) {
         $errors = true;
-    } else {
-        $stmt = $db->query('SELECT id_app FROM application');
-        $fl = false;
-        while ($row = $stmt->fetch()) {
-            if ($row['id_app'] == $_POST['upd_id']) $fl = true;
-        }
-        if (!$fl) {
-            $errors = true;
-            setcookie('upd_id_error', '2', time() + 24 * 60 * 60);
-        }
+        setcookie('upd_id_error', '2', time() + 24 * 60 * 60);
     }
-    if ($errors) {
-        // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
-        header('Location: admin.php');
-        exit();
-    } else {
-        // Удаляем Cookies с признаками ошибок.
-        setcookie('upd_id_error', '', 100000);
-    }
-    setcookie('admin_upd', '1');
-    setcookie('id_upd', $_POST['upd_id']);
-    header('Location: admin.php');
 }
+if ($errors) {
+    // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
+    header('Location: admin.php');
+    exit();
+} else {
+    // Удаляем Cookies с признаками ошибок.
+    setcookie('upd_id_error', '', 100000);
+}
+setcookie('admin_upd', '1');
+setcookie('id_upd', $_POST['upd_id']);
+header('Location: admin.php');

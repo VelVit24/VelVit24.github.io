8f42bd2d7da88b235801a7c3cd513dad117a144c
VelVit24 <vel_vit26@mail.ru>
2024-05-06 11:06:25 +0300

    1

--- web6/admin_upd_form1.php
+++ web6/admin_upd_form1.php
@@ -9,7 +9,7 @@ if (!empty($messages)) {
 ?>
 <form action="" method="POST">
     <label> ID пользователя<br/>
-        <input type="text" name="upd_id" <?php if ($errors['ipd_id']) {print 'class="error"';} ?> >
+        <input type="text" name="upd_id" <?php if ($errors['upd_id']) {print 'class="error"';} ?> >
     </label><br/>
     <input type="submit" name="update1" value="Сохранить">
 </form>

65b6520c7b8fadef6fe7b814161e9de5af64013a
VelVit24 <vel_vit26@mail.ru>
2024-05-14 20:34:22 +0300

    1

--- practice/exit.php
+++ practice/exit.php
@@ -1,15 +1,13 @@
 <?php
-if (isset($_POST['act_exit'])) {
-    session_start();
-    setcookie(session_name(), '', 100000);
-    $_SESSION = array();
-    session_destroy();
-    setcookie('practice_login', '', 100000);
-    setcookie('practice_pass', '', 100000);
-    setcookie('pr_reg_first_name_value', '', 100000);
-    setcookie('pr_reg_last_name_value', '', 100000);
-    setcookie('pr_reg_phone_value', '', 100000);
-    setcookie('pr_reg_email_value', '', 100000);
-    setcookie('pr_reg_date_value', '', 100000);
-    header('Location: ./');
-}
+session_start();
+setcookie(session_name(), '', 100000);
+$_SESSION = array();
+session_destroy();
+setcookie('practice_login', '', 100000);
+setcookie('practice_pass', '', 100000);
+setcookie('pr_reg_first_name_value', '', 100000);
+setcookie('pr_reg_last_name_value', '', 100000);
+setcookie('pr_reg_phone_value', '', 100000);
+setcookie('pr_reg_email_value', '', 100000);
+setcookie('pr_reg_date_value', '', 100000);
+header('Location: ./');

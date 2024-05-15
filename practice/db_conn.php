a018471307a91b250c69ee804b1599a83b56240c
VelVit24 <vel_vit26@mail.ru>
2024-05-15 18:41:42 +0300

    1

--- practice/db_conn.php
+++ practice/db_conn.php
@@ -1,5 +1,5 @@
 <?php
-include('/home/u67330/sql.php');
+include('/home/u67330/www/sql.php');
 $db = new PDO('mysql:host=localhost;dbname=u67330', $user, $pass,
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 ?>

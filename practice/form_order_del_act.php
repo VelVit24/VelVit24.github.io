a76ad998f31665a870ebc8b2e57f7aad044d8bcf
VelVit24 <vel_vit26@mail.ru>
2024-05-16 17:52:38 +0300

    1

--- practice/form_order_del_act.php
+++ practice/form_order_del_act.php
@@ -23,9 +23,9 @@ if ($errors) {
 
 try {
     $stmt = $db->prepare("DELETE pr FROM pr_order_price pr join pr_orders ord on pr.id_order = ord.id_order WHERE pr.id_order = ? and ord.id_user = ?");
-    $stmt->execute([$_POST['id_order'], $_SESSION['practice_login']]);
+    $stmt->execute([$_POST['id_order'], $_SESSION['practice_uid']]);
     $stmt = $db->prepare("DELETE FROM pr_orders WHERE id_order = ? and id_user = ?");
-    $stmt->execute([$_POST['id_order'], $_SESSION['practice_login']]);
+    $stmt->execute([$_POST['id_order'], $_SESSION['practice_uid']]);
 } catch (PDOException $e) {
     echo('Error : ' . $e->getMessage());
     exit();

809a03a7f4e0ae4577e1d34d38a6c47f14029ccf
VelVit24 <vel_vit26@mail.ru>
2024-05-21 12:07:38 +0300

    1

--- web7/index.php
+++ web7/index.php
@@ -167,6 +167,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     include('form.php');
 } // Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 else {
+    print($_COOKIE['token']);
+    $salt = substr($_POST['csrf'],0,4);
+    $secret = $_SESSION['secret'];
+    $token = $salt.':'.md5($salt.':'.$secret);
+    print($token);
     // Проверяем ошибки.
     if (session_start() && !empty($_SESSION['login']) && !empty($_COOKIE[session_name()])) {
         if (!empty($_POST['csrf'])) {
